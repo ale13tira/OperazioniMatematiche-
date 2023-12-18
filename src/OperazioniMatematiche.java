2aee6ce2e03c555f27affec146543a02319b4464
Alessia Tiraboschi <alessia.tiraboschi@itsrizzoli.it>
2023-12-18 16:14:25 +0100

    creata branch somma

--- src/OperazioniMatematiche.java
+++ src/OperazioniMatematiche.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class OperazioniMatematiche {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("inserisci un numero N1: ");
+        int numeroN = scanner.nextInt();
+
+        System.out.println("inserisci un numero N: ");
+        int numeroN2 = scanner.nextInt();
+
+        System.out.println("la somma è uguale a: "+ somma(numeroN, numeroN2));
+
+        scanner.close();
+    }
+
+    public static int somma(int parametro1, int parametro2) {
+        return parametro1 + parametro2;
     }
 }
